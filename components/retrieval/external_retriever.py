270bd89c35d0ebadf455b0617a2aac3e0d5458bf
nk-droid <kumarnidhish22@gmail.com>
2026-04-20 23:43:13 +0530

    added external retriever

--- components/retrieval/external_retriever.py
+++ components/retrieval/external_retriever.py
@@ -1,9 +1,28 @@
 from components.shared_types import RetrievedChunk
 
+from components.shared_types import RetrievedChunk
 from components.retrieval.base_retriever import BaseRetriever
+from langchain_tavily import TavilySearch
+
+from dotenv import load_dotenv
+load_dotenv()
 
 class ExternalRetriever(BaseRetriever):
     """Retrieve context from external systems or APIs."""
 
     def retrieve(self, query: str, top_k: int = 5) -> list[RetrievedChunk]:
-        raise NotImplementedError
+        search_engine = TavilySearch(
+            max_results = top_k,
+            topic = "general"
+        )
+
+        searched_results = search_engine.invoke({"query": query}).get("results", [])
+        
+        return [
+            RetrievedChunk(
+                id=idx,
+                text=result.get("content", ""),
+                score=round(result.get("score", 0), 2),
+                metadata={}
+            ) for idx, result in enumerate(searched_results)
+        ]
