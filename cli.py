36d10d6188423905dc3398bb40a1ab5163a8d088
nk-droid <kumarnidhish22@gmail.com>
2026-05-03 23:01:09 +0530

    updated cli to get source directory and query from the user

--- cli.py
+++ cli.py
@@ -8,30 +8,41 @@ from rich.json import JSON
 console = Console()
 # logger = get_logger(__name__, "DEBUG")
 
+def _get_query():
+    return console.input("[yellow]Enter your query: [/yellow]")
+
+def _get_sources():
+    return console.input("[yellow]Enter the source directory path: [/yellow]")
+
 def main(args):
     config = load_config([
         f"configs/pipeline/{args.pipeline}.yaml",
         f"configs/runtime/{args.runtime}.yaml",
         f"configs/env/{args.env}.yaml"
     ])
-    
-    # console.print(JSON.from_data(config))
 
     orchestrator = RAGOrchestrator(config)
-    state = {
-        "query": "Give one search method with the details of its working. How it can be used in a RAG system?",
-        "sources": "/Users/nidhishkumar/Personal/rag/data/raw/docs"
-    }
+    sources = _get_sources()
+    state = {"sources": sources}
     state = orchestrator.initialize(state)
 
+    state["query"] = _get_query()
+    state["sources"] = sources
+
     state = orchestrator.run(state)
-    print(state["parsed_output"].answer)
+    console.print(f"[green]{state['parsed_output'].answer}[/green]")
+
+    # if args.run_evaluation:
+    #     from scripts.evaluate import run_evaluation
+    #     evaluation_results = run_evaluation(orchestrator, evaluator)
+    #     console.print(JSON.from_data(evaluation_results))
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--pipeline", type=str, help="which pipeline to use")
     parser.add_argument("--runtime", type=str, help="which runtime to use")
     parser.add_argument("--env", type=str, help="which env to use")
+    parser.add_argument("-e", "--eval", action="store_true", help="whether to run evaluation after main")
 
     args = parser.parse_args()
     main(args)
